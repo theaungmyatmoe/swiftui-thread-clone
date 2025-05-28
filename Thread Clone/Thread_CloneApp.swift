53205ef7eb55f48ab38405fe204d4166cdc55f99
Aung Myat Moe <aungmyatmoe834@gmail.com>
2025-05-29 00:41:08 +0630

    Initial Commit

---
+++ Thread Clone/Thread_CloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  Thread_CloneApp.swift
+//  Thread Clone
+//
+//  Created by Aung Myat Moe on 29/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct Thread_CloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
