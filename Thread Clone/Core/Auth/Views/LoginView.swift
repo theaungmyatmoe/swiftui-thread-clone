9660f168d44a9d61eef2cbcb901cfc6049578ff9
Aung Myat Moe <aungmyatmoe834@gmail.com>
2025-05-29 00:43:31 +0630

    init: thread app

---
+++ Thread Clone/Core/Auth/Views/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  Thread Clone
+//
+//  Created by Aung Myat Moe on 29/05/2025.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
